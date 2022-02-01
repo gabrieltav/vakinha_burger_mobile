f938a163efa9f3cf1557f7659b56a78e439d79ba
Gabriel Tavares <tavaresgabriel963@gmail.com>
2022-02-01 14:55:49 -0400

    Buttom

--- front_mobile/lib/app/modules/splash/splash_page.dart
+++ front_mobile/lib/app/modules/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_button.dart';
 import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_textformfield.dart';
 
 class SplashPage extends StatelessWidget {
@@ -15,13 +16,17 @@ class SplashPage extends StatelessWidget {
           const SizedBox(
             height: 20,
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
             child: VakinhaTextformfield(
               label: 'Text Form',
               obscureText: true,
             ),
           ),
+          const SizedBox(
+            height: 20,
+          ),
+          VakinhaButton(label: 'Entrar', onPressed: () {}),
         ],
       ),
     );

90936588a69146c1f58c0ff3642e3f333758f50d
Gabriel Tavares <tavaresgabriel963@gmail.com>
2022-02-01 17:38:50 -0400

    Register Page

--- front_mobile/lib/app/routes/auth_router.dart
+++ front_mobile/lib/app/routes/auth_router.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:vakinha_burger_mobile/app/modules/auth/login/login_page.dart';
+import 'package:vakinha_burger_mobile/app/modules/auth/login/register/register_page.dart';
 
 class AuthRouter {
   AuthRouter._();
@@ -8,6 +9,10 @@ class AuthRouter {
     GetPage(
       name: '/auth/login',
       page: () => LoginPage(),
+    ),
+    GetPage(
+      name: '/auth/register',
+      page: () => const RegisterPage(),
     ),
   ];
 }
